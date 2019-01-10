5dae27bbdc0c03289bfe9c5120b4e02f0ffb3810
WladySer <sergiovillacres3@gmail.com>
2019-01-10 15:32:11 -0500

    cambios 10-01-2019

---
+++ sgoa/aplicacion/modulos_administrador/navbar_adm_profesores.php
@@ -0,0 +1,27 @@
+<nav class="navbar navbar-default">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>                        
+            </button>
+            <a class="navbar-brand" href="#">Bienvenid@: <strong><?php echo $_SESSION['usuario'] ?></strong></a>
+        </div>
+        <div class="collapse navbar-collapse" id="myNavbar">
+            <ul class="nav navbar-nav">
+                <li class="active"><a href="adm_buscar_profesores.php">Gestionar profesores</a></li>
+                <li><a href="adm_buscar_estudiantes.php">Gestionar Estudiantes</a></li>
+                <li><a href="../modulos_comunes/index.php">Foro</a></li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="../../aplicacion/desconectar_sesion.php"><span class="glyphicon glyphicon-log-out"></span> Salir</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+
+
+
+
