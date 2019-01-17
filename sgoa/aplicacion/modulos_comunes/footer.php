3bd9140352b4ed2b2aabcd063703739b4b633e4c
JHyel2015 <elgeorh@gmail.com>
2019-01-16 23:16:03 -0500

    create footer

---
+++ sgoa/aplicacion/modulos_comunes/footer.php
@@ -0,0 +1,3 @@
+<footer class="label-default container-fluid text-center">
+    <p class="copyright small">Copyright &copy; Cevallos Carolina,  Heredia Richard, Pachacama Pamela, Villacres Sergio 2019</p>
+</footer>
